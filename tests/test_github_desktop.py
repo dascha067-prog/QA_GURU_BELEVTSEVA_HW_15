a4bcd8063072efad978c85626bd271cc8b1222c7
Darya Belevtseva <dascha067@gmail.com>
2025-12-19 16:20:05 +0300

    Сделала варианты неподходящих параметров и тестов

--- tests/test_github_desktop.py
+++ tests/test_github_desktop.py
@@ -7,17 +7,20 @@ from selene import browser, have
     [
         (1366, 768),
         (1920, 1080),
+        (390, 844),   # mobile
     ]
 )
 def test_sign_in_desktop(window_size):
     width, height = window_size
+
+    if width < 768:
+        pytest.skip("Mobile resolution is not applicable for desktop test")
+
     browser.config.window_width = width
     browser.config.window_height = height
 
     browser.open("https://github.com/")
 
-    #  click Sign in
     browser.all("a[href*='login']").by(have.text("Sign in")).first.click()
-
-    # verify redirect
     browser.should(have.url_containing("/login"))
+
--- tests/test_github_mobile.py
+++ tests/test_github_mobile.py
@@ -2,6 +2,7 @@ import pytest
 from selene import browser, be
 
 
+@pytest.mark.skip(reason="Mobile layout does not require separate test for Sign in")
 @pytest.mark.parametrize(
     "window_size",
     [
@@ -15,5 +16,5 @@ def test_sign_in_mobile(window_size):
     browser.config.window_height = height
 
     browser.open("https://github.com/")
-
     browser.element("a[href='/login']").should(be.visible).click()
+
